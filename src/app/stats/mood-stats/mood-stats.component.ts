940a4fb084a621038a13d3d01157d3ef972d37c9
Silktrader <silktrader@gmail.com>
2022-05-08 00:09:26 +0200

    Parametrise stat requests with dates, records #

--- src/app/stats/mood-stats/mood-stats.component.ts
+++ src/app/stats/mood-stats/mood-stats.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, combineLatest, combineLatestWith, debounceTime, map, Observable, shareReplay, startWith, tap } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { BehaviorSubject, combineLatest, debounceTime, map, Observable, startWith, tap } from 'rxjs';
+import { DiaryService } from 'src/app/services/diary.service';
 import { StatsService } from '../stats.service';
 
 @Component({
@@ -11,23 +15,30 @@ import { StatsService } from '../stats.service';
 export class MoodStatsComponent implements OnInit {
 
   controls = new FormGroup({
-    records: new FormControl(undefined)
+    records: new FormControl(undefined),
+    startDate: new FormControl(),
+    endDate: new FormControl()
   });
 
   statSelector = new FormControl('nothing');
 
   chosenStat$: Observable<string>;
 
-  highMoodFoodsData;
+  dataSource;
 
   loading$ = new BehaviorSubject<boolean>(false);
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private ss: StatsService) { }
 
   ngOnInit(): void {
 
     // remember previous values; seems necessary to trigger the combineLatest operator
-    this.controls.patchValue({ records: '10' });
+    const date = new Date(Date.now());
+    date.setFullYear(date.getFullYear() - 1);
+    this.controls.patchValue({ records: '15', startDate: date, endDate: new Date(Date.now()) });
     this.statSelector.setValue('nothing');
 
     this.chosenStat$ = combineLatest([
@@ -45,11 +56,17 @@ export class MoodStatsComponent implements OnInit {
 
   fetchData(value: string, parameters) {
     console.log(parameters);
+    if (parameters.startDate === null || parameters.endDate === null)
+      return;
+
+    parameters = { ...parameters, startDate: DiaryService.toDateUrl(parameters.startDate), endDate: DiaryService.toDateUrl(parameters.endDate) };
     if (value === 'high-mood-foods') {
       this.loading$.next(true);
       this.ss.getHighMoodFoods(parameters).subscribe({
         next: data => {
-          this.highMoodFoodsData = data.foods;
+          this.dataSource = new MatTableDataSource(data.foods);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
           this.loading$.next(false);
         },
         error: error => console.log('issue fetching stat')
